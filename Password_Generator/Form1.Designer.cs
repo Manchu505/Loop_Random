4c12d3ad935b67898ee79baa0904fb5d3a17b92f
Manchu505 <19881829+Manchu505@users.noreply.github.com>
2023-03-31 09:29:26 -0500

    Add project files.

---
+++ Password_Generator/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Password_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.GenterateButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(59, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter the number of Characters";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tempus Sans ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(53, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(326, 27);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "How many password to generate";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // GenterateButton
+            // 
+            this.GenterateButton.Location = new System.Drawing.Point(179, 412);
+            this.GenterateButton.Name = "GenterateButton";
+            this.GenterateButton.Size = new System.Drawing.Size(75, 23);
+            this.GenterateButton.TabIndex = 4;
+            this.GenterateButton.Text = "Save to File";
+            this.GenterateButton.UseVisualStyleBackColor = true;
+            this.GenterateButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(51, 202);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(331, 190);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.GenterateButton);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(191, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(424, 498);
+            this.panel1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 412);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(789, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.quitToolStripMenuItem.Text = "Exit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(789, 542);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button GenterateButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
+
---
+++ Password_Generator/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Password_Generator
+{
+    public partial class Form1 : Form
+    {
+        // Setting variables and the Random class to be used throughout form.
+        int currentpasswordLength = 0;
+        int howmanyPW;
+        Random character = new Random();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //changing up elemets on the form for astetics 
+            panel1.BackColor = Color.FromArgb(100, 0, 0, 0);
+        }
+        
+        private void passwordGenerator(int passwordLength)
+        {
+            // this is the for loop to actaully create the passwords
+            // I created mulitple types of string input incase I want to change how passwords are created later
+            string CapitalLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            string SmallLetters = "qwertyuiopasdfghjklzxcvbnm";
+            string Digits = "0123456789";
+            string SpecialCharacters = "!@#$%^&*()-_=+<,>.";
+            string AllChar = CapitalLetters + SmallLetters + Digits + SpecialCharacters;
+
+            string randomPassword = "";
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int randomNum = character.Next(0, AllChar.Length);
+                randomPassword += AllChar[randomNum];
+            }
+            
+            // Prints the passwords to a rich text box
+            richTextBox1.AppendText(randomPassword + "\n");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // this is the loop for generating more than one password at a time
+            // good example of do-while loop
+            int c = 1;
+            int howmanyPW = int.Parse(textBox2.Text);
+
+            try
+            {
+                do 
+                {
+                    int passwordLengthinput = int.Parse(textBox1.Text);
+                    
+                    currentpasswordLength = passwordLengthinput;
+                    passwordGenerator(currentpasswordLength);
+                    c++;
+                }
+                while (c <= howmanyPW);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("try again");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Save to text file - currently saving to /bin/debug
+            StreamWriter sf = new StreamWriter("password.txt", true);
+            sf.WriteLine(richTextBox1.Text);
+            sf.Flush();
+            sf.Close(); 
+
+            
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Close the application
+            this.Close();
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //Clear all boxes
+            richTextBox1.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+        }
+    }
+}
+
